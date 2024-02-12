e14faab8db3e800a5b092cfa728921f2c278a0a8
jancoenjonas <74824237+jancoenjonas@users.noreply.github.com>
2024-02-12 17:28:55 +0100

    Update data_to_flask.cpp

--- week 2/arduino/data_to_flask.cpp
+++ week 2/arduino/data_to_flask.cpp
@@ -2,17 +2,22 @@
 #include <HTTPClient.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include "secret.h" 
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include "secret.h"
 
 #define RST_PIN 26 
 #define SS_PIN 25  
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Adjust the I2C address if necessary
 
 void sendDataToFlaskServer(const char* data) {
   WiFiClient client;
   if (!client.connect(SECRET_FLASK_SERVER, SECRET_FLASK_PORT)) {
     Serial.println("Connection to Flask server failed");
+    lcd.clear();
+    lcd.print("Server Verbinding Mislukt");
     return;
   }
   
@@ -24,22 +29,55 @@ void sendDataToFlaskServer(const char* data) {
                "Host: " + SECRET_FLASK_SERVER + "\r\n" +
                "Connection: close\r\n\r\n");
 
+  while(client.connected() || client.available()) {
+    if (client.available()) {
+      String line = client.readStringUntil('\n');
+      if (line == "\r") {
+        break;  // Headers received, next line is body
+      }
+    }
+  }
+
+  String response = client.readStringUntil('\n');
+  displayUserData(response);
+
   delay(10);
   client.stop();
+}
+
+void displayUserData(const String& data) {
+  int firstCommaIndex = data.indexOf(',');
+  int secondCommaIndex = data.indexOf(',', firstCommaIndex + 1);
+
+  String streak = data.substring(0, firstCommaIndex);
+  String longestStreak = data.substring(firstCommaIndex + 1, secondCommaIndex);
+  String name = data.substring(secondCommaIndex + 1);
+
+  lcd.clear();
+  lcd.print("Streak: " + streak);
+  lcd.setCursor(0, 1); // Move to the second line
+  lcd.print("Langste: " + longestStreak);
+  // Add additional lines or scrolling if necessary
 }
 
 void setup() {
   Serial.begin(115200);
   SPI.begin(); 
   mfrc522.PCD_Init(); 
+  lcd.init();
+  lcd.backlight();
 
   WiFi.begin(SECRET_SSID, SECRET_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    lcd.clear();
+    lcd.print("Verbinden met WiFi");
   }
   Serial.println("");
   Serial.println("WiFi connected.");
+  lcd.clear();
+  lcd.print("WiFi Verbonden");
 }
 
 void loop() {
@@ -50,9 +88,14 @@ void loop() {
       content.concat(String(mfrc522.uid.uidByte[i], HEX));
     }
 
-    Serial.print("NFC UID:");
+    Serial.print("NFC UID: ");
     Serial.println(content);
+    lcd.clear();
+    lcd.print("NFC Gescand");
     sendDataToFlaskServer(content.c_str());
+  } else {
+    lcd.clear();
+    lcd.print("Wachten op NFC");
   }
-  delay(1000); // Adjust delay as needed
+  delay(1000);
 }
